4f0096b6ec0ee6cdb52e7fc9c73806f09240e2da
user <user@users-MacBook-Pro.local>
2023-02-23 21:35:11 +0100

    finished marking up graphic design page

--- pages/graphic-design.tsx
+++ pages/graphic-design.tsx
@@ -4,6 +4,7 @@ import Project from "@/components/Project";
 import change from "../assets/graphic-design/desktop/image-change.jpg";
 import boxedWater from "../assets/graphic-design/desktop/image-boxed-water.jpg";
 import science from "../assets/graphic-design/desktop/image-science.jpg";
+
 const graphicDesign = () => {
   return (
     <div>
@@ -15,20 +16,21 @@ const graphicDesign = () => {
         <Project
           src={change}
           alt="change"
-          title="the brown"
-          info="A multi-carrier shipping website for ecommerce businesses"
+          title="tim brown"
+          info="A book cover designed for 
+          Tim Brown&lsquo;s new release, &rsquo;Change&rsquo;"
         />
         <Project
           src={boxedWater}
           alt="boxedWater"
-          title="boxedWater"
-          info="A multi-carrier shipping website for ecommerce businesses"
+          title="boxed water"
+          info="A simple packaging concept made for Boxed Water"
         />
         <Project
           src={science}
           alt="science"
           title="science!"
-          info="A multi-carrier shipping website for ecommerce businesses"
+          info="A poster made in collaboration with the Federal Art Project"
         />
       </div>
     </div>
