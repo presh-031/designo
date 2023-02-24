8678d447c44fcbc06cbfa74f4582de746937e6ff
user <user@users-MacBook-Pro.local>
2023-02-24 18:03:27 +0100

    onto styling about page

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,7 +6,7 @@ import Nav from "./Nav";
 
 const Header = () => {
   return (
-    <header className="flex justify-between items-center px-[2.4rem] py-[3.9rem] ">
+    <header className="flex items-center justify-between px-[2.4rem] py-[3.9rem] outline ">
       <div className="">
         <Image src={logo} alt="designo-logo" width={202} height={27} />
       </div>
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,7 +1,62 @@
-import React from "react";
+import AllLocations from "@/components/AllLocations";
+import Image from "next/image";
 
 const about = () => {
-  return <div>about</div>;
+  return (
+    <div>
+      <div>
+        <div>
+          <p>About Us</p>
+          <p>
+            Founded in 2010, we are a creative agency that produces lasting
+            results for our clients. We’ve partnered with many startups,
+            corporations, and nonprofits alike to craft designs that make real
+            impact. We’re always looking forward to creating brands, products,
+            and digital experiences that connect with our clients’ audiences.
+          </p>
+        </div>
+        <div></div>
+      </div>
+
+      <div>
+        <div></div>
+        <div>
+          <p>World-class talent</p>
+          <p>
+            We are a crew of strategists, problem-solvers, and technologists.
+            Every design is thoughtfully crafted from concept to launch,
+            ensuring success in its given market. We are constantly updating our
+            skills in a myriad of platforms.
+          </p>
+          <p>
+            Our team is multi-disciplinary and we are not merely interested in
+            form — content and meaning are just as important. We give great
+            importance to craftsmanship, service, and prompt delivery. Clients
+            have always been impressed with our high-quality outcomes that
+            encapsulates their brand’s story and mission.
+          </p>
+        </div>
+      </div>
+
+      <AllLocations />
+
+      <div>
+        <p>The real deal</p>
+        <p>
+          As strategic partners in our clients’ businesses, we are ready to take
+          on any challenge as our own. Solving real problems require empathy and
+          collaboration, and we strive to bring a fresh perspective to every
+          opportunity. We make design and technology more accessible and give
+          you tools to measure success.
+        </p>
+        <p>
+          We are visual storytellers in appealing and captivating ways. By
+          combining business and marketing strategies, we inspire audiences to
+          take action and drive real results.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default about;
