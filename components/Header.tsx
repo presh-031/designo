c04e526a72c746ce4727c8e919ee2849fb39ca8b
presh-031 <akingbulugbe.dev@gmail.com>
2023-02-28 15:18:47 +0100

    feat: making nav more interactive

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import { useState } from "react";
 
 import logo from "../assets/shared/desktop/logo-dark.png";
@@ -15,8 +16,9 @@ const Header = () => {
 
   return (
     <header className="flex items-center justify-between px-[2.4rem] py-[3.9rem]  ">
-      <Image src={logo} alt="designo-logo" width={202} height={27} />
-
+      <Link href="/">
+        <Image src={logo} alt="designo-logo" width={202} height={27} />
+      </Link>
       <div onClick={handleMenuClick}>
         {isOpen ? (
           <Image src={closeMenu} alt="menu" width={24} height={20} />
@@ -24,7 +26,7 @@ const Header = () => {
           <Image src={hamburgerMenu} alt="menu" width={24} height={20} />
         )}
       </div>
-      {isOpen && <Nav />}
+      {isOpen && <Nav setIsOpen={setIsOpen} />}
     </header>
   );
 };
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,8 +1,15 @@
 import Link from "next/link";
 
-const Nav = () => {
+type NavProps = {
+  setIsOpen: (isOpen: boolean) => boolean;
+};
+const Nav = ({ setIsOpen }: NavProps) => {
+  const handleNavItemClick = () => {
+    setIsOpen((prevIsOpen) => !prevIsOpen);
+  };
+
   return (
-    <nav className="absolute top-[10.5rem] right-0 bottom-0 z-10 w-full bg-black bg-opacity-50 text-[2.4rem] font-normal leading-[2.5rem] tracking-[0.2rem] text-white ">
+    <nav className="absolute top-[10.5rem] right-0 bottom-0 z-[10000] w-full bg-black bg-opacity-50 text-[2.4rem] font-normal leading-[2.5rem] tracking-[0.2rem] text-white ">
       <ul className="flex flex-col gap-[3.2rem] bg-black px-[2.4rem] py-[4.8rem]">
         <li>
           <Link href="/about">OUR COMPANY</Link>
