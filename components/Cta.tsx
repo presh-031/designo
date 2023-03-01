6b496a1645c8d765447152e96213f4161521b00f
presh-031 <akingbulugbe.dev@gmail.com>
2023-03-01 15:05:54 +0100

    still adjusting bg image for otherpageshero

--- components/Cta.tsx
+++ components/Cta.tsx
@@ -2,7 +2,7 @@ import WhiteButton from "./WhiteButton";
 
 const Cta = () => {
   return (
-    <div className="relative top-[18.95rem] mx-[2.4rem] mt-[-6.85rem]  rounded-2xl bg-peach px-[2.4rem] py-[6.4rem] text-center text-white">
+    <div className="relative top-[18.95rem] mx-[2.4rem] mt-[-6.85rem] rounded-[1.5rem]  bg-peach bg-[url('../assets/shared/desktop/bg-pattern-call-to-action.svg')] bg-right  px-[2.4rem] py-[6.4rem] text-center text-white">
       <p className="mb-[0.6rem] text-[3.2rem] font-medium leading-[3.6rem]">
         Let&apos;s talk about your project
       </p>
--- components/OtherPagesHero.tsx
+++ components/OtherPagesHero.tsx
@@ -4,7 +4,7 @@ type OtherPagesHeroProps = {
 };
 const OtherPagesHero = (props: OtherPagesHeroProps) => {
   return (
-    <div className="bg-peach py-[10.5rem] px-[2.4rem]  text-center text-white">
+    <div className="bg-peach bg-[url('../assets/shared/desktop/bg-pattern-small-circle.svg')] bg-contain bg-[right_-2rem_-3.5rem] bg-no-repeat py-[10.5rem] px-[2.4rem]  text-center text-white">
       <p className="mb-[2.4rem] text-[3.2rem] font-normal leading-[3.6rem]">
         {props.title}
       </p>
