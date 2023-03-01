e2074289a713afded2f6e9d52d955050050234cb
presh-031 <akingbulugbe.dev@gmail.com>
2023-03-01 07:56:35 +0100

    feat: added smooth scroll

--- components/SeeLocation.tsx
+++ components/SeeLocation.tsx
@@ -9,9 +9,19 @@ type seeLocationProps = {
 
 const SeeLocation = (props: seeLocationProps) => {
   return (
-    <div className="text-center font-normal ">
-      <div className="mx-auto mb-[4.8rem] grid  h-[20.2rem] w-[20.2rem] place-items-center rounded-full outline ">
-        <Image src={props.src} alt={props.location} />
+    <div className=" border border-red-800 text-center font-normal ">
+      <div
+        className=" mx-auto mb-[4.8rem]  grid h-[20.2rem] w-[20.2rem]  rounded-full   outline "
+        style={{
+          background:
+            "linear-gradient(180deg, rgba(93,2,2,0) 0%, rgba(93,2,2,0.005) 100%)",
+        }}
+      >
+        <Image
+          className="border border-red-800"
+          src={props.src}
+          alt={props.location}
+        />
       </div>
       <p className="mb-[3.2rem] text-[2rem]  uppercase leading-[2.6rem] tracking-[0.5rem]  text-dark-grey">
         {props.location}
@@ -19,7 +29,7 @@ const SeeLocation = (props: seeLocationProps) => {
       <Link
         href={`/locations/#${props.href}`}
         scroll={false}
-        className="rounded-2xl bg-peach p-[1.8rem] text-[1.5rem] uppercase leading-[2.1rem]  tracking-[0.1rem] text-white"
+        className="inline-block rounded-2xl bg-peach p-[1.8rem] text-[1.5rem] uppercase leading-[2.1rem]  tracking-[0.1rem] text-white"
       >
         see location
       </Link>
