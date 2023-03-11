a1b7c564cb983e6e70b4595f71ebd12308b1e26c
presh-031 <akingbulugbe.dev@gmail.com>
2023-03-11 09:46:36 +0100

    fix: added types for otherprojecttypescard props

--- ui/OtherProjectTypesCard.tsx
+++ ui/OtherProjectTypesCard.tsx
@@ -1,4 +1,7 @@
-const OtherProjectTypesCard = ({ children }) => {
+type OtherProjectTypesCardProps = {
+  children: React.ReactNode;
+};
+const OtherProjectTypesCard = ({ children }: OtherProjectTypesCardProps) => {
   return (
     <div className="mt-[9.6rem] mb-[-9.4rem] flex flex-col gap-[2.4rem]  px-[2.4rem]  sm:mt-[12rem] sm:px-[4rem]">
       {children}
