ee4fdff607bc000dab5062860b9f3166afa7cdf7
presh-031 <akingbulugbe.dev@gmail.com>
2023-03-28 06:12:57 +0100

    test: testing styles in projects pages

--- pages/web-design.tsx
+++ pages/web-design.tsx
@@ -3,7 +3,6 @@ import OtherPagesHero from "@/components/OtherPagesHero";
 import Project from "@/components/Project";
 import ProjectType from "@/components/ProjectType";
 import OtherProjectTypesCard from "@/ui/OtherProjectTypesCard";
-import ProjectsCard from "@/ui/ProjectsCard";
 import blogr from "../assets/web-design/desktop/image-blogr.jpg";
 import builder from "../assets/web-design/desktop/image-builder.jpg";
 import camp from "../assets/web-design/desktop/image-camp.jpg";
@@ -19,7 +18,8 @@ const webDesign = () => {
         aboutTitle="We build websites that serve as powerful marketing tools and bring memorable brand experiences."
       />
 
-      <ProjectsCard>
+      {/* <ProjectsCard> */}
+      <div className="mb-[9.6rem] grid grid-cols-1 gap-[4rem]  sm:mb-[12rem] sm:gap-[3.2rem] sm:px-[3.9rem] xl:mb-[16rem] xl:grid-cols-3  xl:gap-[3rem] xl:px-[16.5rem]">
         <Project
           src={express}
           alt="express"
@@ -56,8 +56,8 @@ const webDesign = () => {
           title="camp"
           info="A multi-carrier shipping website for ecommerce businesses"
         />
-      </ProjectsCard>
-      {/* </div> */}
+        {/* </ProjectsCard> */}
+      </div>
 
       <OtherProjectTypesCard>
         <ProjectType
